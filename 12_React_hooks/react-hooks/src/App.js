9c561b9a36d9d9166c1e451d9f7e36a10079107c
Trinh Gliedt <chuot2008@gmail.com>
2021-07-25 00:08:55 -0700

    useMemo

--- 12_React_hooks/react-hooks/src/App.js
+++ 12_React_hooks/react-hooks/src/App.js
@@ -1,61 +1,50 @@
 import "./App.css";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
 // to test, copy and paste entire file to App.js
 function App() {
-  const [name, setName] = useState("");
-  // the useState for renderCount below is just an example to use with the useEffect to set renderCount
-  // const [renderCount, setRenderCount] = useState(0);
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  // First use case:
+  // Every time the application re-render, whether the input or theme changed, the slow function got called, and this causes a big performance issue.
+  // In order to avoid re-running functions that don't change, React has a way to solve this by using useMemo. Memo stands for memoization, which is a way for caching a value so that you don't have to recompute it every time.
+  // In our example here, slow function takes an input of number and it will give us the same output if the input is the same. So we can cache that input value number and the output it gives us, that way is the input doesn't change, we don't have to recalculate our slow function over and over again.
+  // This is the old way without using useMemo
+  // const doubleNumber = slowFunction(number);
+  // This is to use useMemo:
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+    // number is the dependency of this useMemo. If our number change, we'll rerun the code in this useMemo. But if our number doesn't change we don't need to rerun the code
+    // So now with this, when we click change input, the slow function will run, but when we click change theme, the slow function won't run
+    // some cons of useMemo: the reason why we don't want to use this for everything is that it require memory overhead and performance overhead.  For example, this useMemo function must be called every single render of your component (so you're calling additional function), and it's saving the previous value in some memory variables, so you're forcing your memory to get larger every time you useMemo. This isn't a huge deal, but if you use it everywhere in your application it would require more function run and memory where not using it would have been better. So we should just use it for this use case, when the function you call is incredibly slow
+  }, [number]);
 
-  // Use case #1:
-  // useRef can be used to store information that persists through re-render, but it doesn't cause a re-render like state does
-  // useRef can take a default value, we're using 0 as below
-  const renderCount = useRef(1);
-  // useRef will return an object with a key named current. And the initial value will be set to current. Like this: {current: 0}
-  // when we update the current property, that's what's persisted between renders
+  const themeStyles = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
 
-  useEffect(() => {
-    renderCount.current = renderCount.current + 1;
-  });
-
-  // the useEffect below will run an infinite loop, the component just keep rendering again and again. Which  is why useState is not a good choice to count render.
-  // useEffect(() => {
-  //   setRenderCount((prevRenderCount) => prevRenderCount + 1);
-  // });
-
-  // Use case #2:
-  // a very common use case for ref is to move focus to an element (like an input)
-  const inputRef = useRef();
-  function focus() {
-    inputRef.current.focus();
-    // this focus way is so common that sometimes people would update the value of the input like below:
-    inputRef.current.value = "Some value";
-    // While the value of the inputRef changes, state is not updated, as we can see "My name is" is still blank..
-    // the advice is, not to use Ref to update input or append child element this way, and to do it through states instead
-  }
-
-  // Use case 3: is to store the previous value of state: because, there's no way to get previous value of state, it's always what state is..
-  const prevName = useRef("");
-  useEffect(() => {
-    prevName.current = name;
-    // this is a way to save previous state without causing additional re-render that we don't actually need.
-  }, [name]);
-
+  // Second use case: referencial equality
   return (
     <div className="App" style={{ margin: "3rem" }}>
       <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        ref={inputRef}
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
       />
-      <button onClick={focus}>Focus</button>
-      <h1>
-        My name is {name} and it used to be {prevName.current}
-      </h1>
-      <h2>I rendered {renderCount.current} times</h2>
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change Theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
     </div>
   );
 }
 
 export default App;
+// The slow function below is just to demonstrate bad case scenario when there are slow functions in the application and how they can affect run time for re-rendering components
+function slowFunction(num) {
+  console.log("Calling slow function");
+  for (let i = 0; i <= 1000000000000; i++) {
+    return num * 2;
+  }
+}
