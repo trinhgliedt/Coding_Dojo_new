a2126d3f586a357b384258ba599ae3d3468214d6
Trinh Gliedt <chuot2008@gmail.com>
2021-07-29 16:35:25 -0700

    clean up

--- 06_Post_Dojo_Projects/01_Expense_approval/client/src/views/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const NotFound = (props) => {
-  console.log(props);
-  return <div>Page Not Found ...</div>;
-};
-
-export default NotFound;
--- 10_NextJS/first_next_js/pages/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function HomePage() {
-  return (
-    <div className={styles.container}>
-      <h1>Home page</h1>
-    </div>
-  );
-}
